2d9e4da61581b0f96fc251e9314dc671af12c7c3
Mika Reich <mikareich0@gmail.com>
2025-07-30 14:28:16 +0200

    fix: upload db to public folder

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,10 +1,10 @@
 import { createClient } from '@libsql/client'
 import { drizzle } from 'drizzle-orm/libsql'
 
-const path =
-  process.env.NODE_ENV === 'development'
-    ? './public/dreimetadaten.db'
-    : `https://${process.env.VERCEL_URL}/dreimetadaten.db`
+const path = './public/dreimetadaten.db'
+// process.env.NODE_ENV === 'development'
+//   ? './public/dreimetadaten.db'
+//   : `https://${process.env.VERCEL_URL}/dreimetadaten.db`
 
 const client = createClient({ url: `file:${path}` })
 export const db = drizzle(client)
