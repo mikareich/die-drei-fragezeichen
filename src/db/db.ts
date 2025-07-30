dac96438ed1c57b06e9841e629fe1c9ee0d9e1da
Mika Reich <mikareich0@gmail.com>
2025-07-30 14:25:16 +0200

    fix: update db path

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,7 +1,10 @@
 import { createClient } from '@libsql/client'
 import { drizzle } from 'drizzle-orm/libsql'
 
-const path = './src/db/dreimetadaten.db'
+const path =
+  process.env.NODE_ENV === 'development'
+    ? './public/dreimetadaten.db'
+    : `https://${process.env.VERCEL_URL}/dreimetadaten.db`
 
 const client = createClient({ url: `file:${path}` })
 export const db = drizzle(client)
