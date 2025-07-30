d2bc1da512ca3915c1ca76777f452f1ab6aa8b23
Mika Reich <mikareich0@gmail.com>
2025-07-30 16:30:52 +0200

    chore: remove unused db files

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from "drizzle-kit";
+import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
-	dbCredentials: {
-		url: "./src/db/dreimetadaten.db",
-	},
-	dialect: "sqlite",
-	out: "./src/db/drizzle",
-	schema: "./src/db/schema.ts",
-	// schemaFilter: ['serie'],
-	// tablesFilter: ['*'],
-	verbose: true,
-});
+  dbCredentials: {
+    url: './public/dreimetadaten.db',
+  },
+  dialect: 'sqlite',
+  out: './src/db/drizzle',
+  schema: './src/db/schema.ts',
+  // schemaFilter: ['serie'],
+  // tablesFilter: ['*'],
+  verbose: true,
+})
