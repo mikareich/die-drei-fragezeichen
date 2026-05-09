34d69eeae652a7fc1ce6b23e31230f9f1c1dd0b0
Mika Reich <mikareich0@gmail.com>
2026-05-09 21:45:58 +0200

    chore: fmt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,17 @@
-import './globals.css'
-import { Geist_Mono } from 'next/font/google'
+import "./globals.css";
+import { Geist_Mono } from "next/font/google";
 
 const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
-type RootLayoutProps = { children: React.ReactNode }
+type RootLayoutProps = { children: React.ReactNode };
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="de" className={`${geistMono.variable} antialiased`}>
       <body>{children}</body>
     </html>
-  )
+  );
 }
