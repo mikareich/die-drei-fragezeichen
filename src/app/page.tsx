673204838286ee310d927b28e2f0f22dc444ebda
Mika Reich <mikareich0@gmail.com>
2026-06-06 19:35:28 +0200

    fix(homepage): add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,43 +5,57 @@ import { DataTable } from "~/components/DataTable.tsx";
 
 export default function HomePage(): React.ReactNode {
   return (
-    <DataTable className="grid-cols-[auto_auto_1fr_auto]">
-      <DataTable.Header>
-        <DataTable.Item>Nummer</DataTable.Item>
-        <DataTable.Item>Die Drei ???</DataTable.Item>
-        <DataTable.Item>Beschreibung</DataTable.Item>
-      </DataTable.Header>
+    <main>
+      <section className="mb-8">
+        <h1 className="text-h2 mb-4">Episodenverzeichnis</h1>
+        <p className="text-lg">
+          Willkommen im ???-Archiv. Hier findest du alle Folgen der drei
+          Detektive auf einen Blick – übersichtlich, durchsuchbar und sortiert.
+          Egal ob du alte Klassiker wiederentdecken oder aktuelle Fälle
+          nachholen willst, hier wirst du fündig.
+        </p>
+      </section>
 
-      <DataTable.Row>
-        <DataTable.Item>001</DataTable.Item>
+      <DataTable className="grid-cols-[auto_1fr_auto] sm:grid-cols-[auto_auto_1fr_auto]">
+        <DataTable.Header>
+          <DataTable.Item>Nummer</DataTable.Item>
+          <DataTable.Item>Die Drei ???</DataTable.Item>
+          <DataTable.Item className="not-sm:hidden">
+            Beschreibung
+          </DataTable.Item>
+        </DataTable.Header>
 
-        <DataTable.Item isTitle={true}>und der Super Papagei</DataTable.Item>
+        <DataTable.Row>
+          <DataTable.Item>001</DataTable.Item>
 
-        <DataTable.Item>
-          Der Auftrag an die drei Detektive hört sich recht harmlos an: Sie
-          sollen einen entflogenen Papagei suchen. Doch kaum beginnen sie mit
-          ihren Nachforschungen, da scheinen sich plötzlich noch einige andere
-          Leute sehr für diesen Papagei zu interessieren. Vielleicht deshalb,
-          weil er lateinische Sprüche zitieren kann? Aber bald geht es nicht
-          mehr nur um einen, sondern um sieben Papageien – und alle sieben
-          führen höchst seltsame Reden. Ob da nicht eine geheime Botschaft
-          dahintersteckt? Jedenfalls sind auch ein jähzorniger Kunsthändler und
-          ein berüchtigter Meisterdieb hinter den Vögeln her. Die drei ???
-          müssen sich ganz schön die hellen Köpfe zerbrechen, ehe sie diesen
-          abenteuerlichen Fall aufklären und eine wohlverdiente Belohnung
-          einheimsen
-        </DataTable.Item>
+          <DataTable.Item isTitle={true}>und der Super Papagei</DataTable.Item>
 
-        <DataTable.Item>
-          <Button
-            mode="outlined"
-            asChild={true}
-            suffixIcon={<ArrowRightIcon />}
-          >
-            <Link href="#">Zur Folge</Link>
-          </Button>
-        </DataTable.Item>
-      </DataTable.Row>
-    </DataTable>
+          <DataTable.Item className="not-sm:hidden">
+            Der Auftrag an die drei Detektive hört sich recht harmlos an: Sie
+            sollen einen entflogenen Papagei suchen. Doch kaum beginnen sie mit
+            ihren Nachforschungen, da scheinen sich plötzlich noch einige andere
+            Leute sehr für diesen Papagei zu interessieren. Vielleicht deshalb,
+            weil er lateinische Sprüche zitieren kann? Aber bald geht es nicht
+            mehr nur um einen, sondern um sieben Papageien – und alle sieben
+            führen höchst seltsame Reden. Ob da nicht eine geheime Botschaft
+            dahintersteckt? Jedenfalls sind auch ein jähzorniger Kunsthändler
+            und ein berüchtigter Meisterdieb hinter den Vögeln her. Die drei ???
+            müssen sich ganz schön die hellen Köpfe zerbrechen, ehe sie diesen
+            abenteuerlichen Fall aufklären und eine wohlverdiente Belohnung
+            einheimsen
+          </DataTable.Item>
+
+          <DataTable.Item>
+            <Button
+              mode="outlined"
+              asChild={true}
+              suffixIcon={<ArrowRightIcon />}
+            >
+              <Link href="#">Zur Folge</Link>
+            </Button>
+          </DataTable.Item>
+        </DataTable.Row>
+      </DataTable>
+    </main>
   );
 }
